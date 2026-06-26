4ad175cd851075be340aa31edebfebf45a0bc43a
Davox1095 <davidmorag1095@gmail.com>
2026-06-26 16:32:45 -0600

    Se ajusto la configuracion de base de datos

--- app/config/database.py
+++ app/config/database.py
@@ -1,15 +1,7 @@
-import os
-
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Conexion para MySQL usando WAMP.
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "mysql+pymysql://root:@localhost:3306/sistema_medicamentos_db"
-)
-
+DATABASE_URL = "mysql+pymysql://root:@localhost:3306/sistema_medicamentos_db"
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine)
 
@@ -17,7 +9,6 @@ Base = declarative_base()
 
 
 def init_db():
-    # Importa las entidades para que SQLAlchemy las registre.
     from app.entity.usuario import UsuarioORM
     from app.entity.centro_recepcion import CentroRecepcionORM
     from app.entity.medicamento import MedicamentoORM
@@ -25,4 +16,4 @@ def init_db():
     from app.entity.solicitud import SolicitudORM, DetalleSolicitudORM
     from app.entity.entrega import EntregaORM
 
-    Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(engine)
