37e4b49f983d03f95308e82e2cf291e39fa3ae8a
Davox1095 <davidmorag1095@gmail.com>
2026-06-27 10:11:52 -0600

    Se agrego el UML y ajustes de configuracion

--- sql/crear_base_datos.sql
+++ sql/crear_base_datos.sql
@@ -1,3 +1,5 @@
-CREATE DATABASE IF NOT EXISTS sistema_medicamentos_db;
+CREATE DATABASE IF NOT EXISTS sistema_medicamentos_db
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
 USE sistema_medicamentos_db;
