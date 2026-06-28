ee2b400065fe23197121eaca19a2fda682b775a7
Davox1095 <davidmorag1095@gmail.com>
2026-06-28 09:54:33 -0600

    Se agrego validacion de correo registrado

--- app/service/usuario_service.py
+++ app/service/usuario_service.py
@@ -20,8 +20,15 @@ class UsuarioService:
         if rol not in ROLES_USUARIO:
             raise ValueError("Rol no permitido")
 
+    def validar_correo(self, correo, id_usuario=None):
+        usuario = self.repo.get_by_correo(correo)
+
+        if usuario and usuario.id_usuario != id_usuario:
+            raise ValueError("Correo ingresado ya esta registrado")
+
     def create_usuario(self, id_usuario, nombre, correo, contrasena, rol, activo=True):
         self.validar_rol(rol)
+        self.validar_correo(correo)
         return self.repo.create(id_usuario, nombre, correo, contrasena, rol, activo)
 
     def get_usuario(self, id_usuario):
@@ -32,6 +39,7 @@ class UsuarioService:
 
     def update_usuario(self, id_usuario, nombre, correo, contrasena, rol, activo=True):
         self.validar_rol(rol)
+        self.validar_correo(correo, id_usuario)
         return self.repo.update(id_usuario, nombre, correo, contrasena, rol, activo)
 
     def delete_usuario(self, id_usuario):
